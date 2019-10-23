bed73025dd5531d65766174034d5b3842b898922
Faiq <faiq.kaboel@gmail.com>
2019-10-23 23:45:02 +0700

    Note construct and setter

--- app/src/main/java/com/example/nottaker/Note.java
+++ app/src/main/java/com/example/nottaker/Note.java
@@ -1,5 +1,35 @@
 package com.example.nottaker;
 
+import androidx.annotation.NonNull;
+
 public class Note {
 
+    String id, title, content, date, time;
+
+    Note() {};
+
+    Note(String title, String content, String date, String time) {
+
+        this.title = title;
+        this.content = content;
+        this.date = content;
+        this.time = content;
+
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
