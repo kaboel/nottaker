4c44cdfa54d56b89058cd4547d0789b272f492f2
Faiq <faiq.kaboel@gmail.com>
2019-10-23 23:47:09 +0700

    getter and setter

--- app/src/main/java/com/example/nottaker/Note.java
+++ app/src/main/java/com/example/nottaker/Note.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 
 public class Note {
 
-    String id, title, content, date, time;
+    private String id, title, content, date, time;
 
     Note() {};
 
@@ -17,16 +17,50 @@ public class Note {
 
     }
 
+    Note(String id, String title, String content, String date, String time) {
+
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.date = content;
+        this.time = content;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getContent() {
+        return content;
     }
 
     public void setContent(String content) {
         this.content = content;
     }
 
+    public String getDate() {
+        return date;
+    }
+
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public String getTime() {
+        return time;
     }
 
     public void setTime(String time) {
