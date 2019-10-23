2059d7f3471c61c72d2a5f1d72d9159a4c4a7868
Faiq <faiq.kaboel@gmail.com>
2019-10-24 00:09:48 +0700

    call add note activity

--- app/src/main/java/com/example/nottaker/MainActivity.java
+++ app/src/main/java/com/example/nottaker/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.nottaker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +24,23 @@ public class MainActivity extends AppCompatActivity {
         this.toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(this.toolbar);
         this.recView = findViewById(R.id.noteList);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.addBtn) {
+            Intent intent = new Intent(this, AddNote.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
