33d7a0be3ef7796f4c1d7e7da8c1fa63cac581de
Faiq <faiq.kaboel@gmail.com>
2019-10-24 03:09:37 +0700

    attach adapter to recyclerview

--- app/src/main/java/com/example/nottaker/MainActivity.java
+++ app/src/main/java/com/example/nottaker/MainActivity.java
@@ -3,18 +3,24 @@ package com.example.nottaker;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     RecyclerView recView;
+    Adapter adapter;
+    List<Note> notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(this.toolbar);
+
+        NoteDB con = new NoteDB(this);
+        this.notes = con.getNotes();
         this.recView = findViewById(R.id.noteList);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        this.recView.setLayoutManager(llm);
+        this.adapter = new Adapter(this, this.notes);
+        this.recView.setAdapter(this.adapter);
+
     }
 
     @Override
