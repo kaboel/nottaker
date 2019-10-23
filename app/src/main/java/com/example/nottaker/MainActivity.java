9c1aeca8f7a95760facd59e4a63326e13d1ab9b3
Faiq <faiq.kaboel@gmail.com>
2019-10-23 23:25:45 +0700

    Design layout

--- app/src/main/java/com/example/nottaker/MainActivity.java
+++ app/src/main/java/com/example/nottaker/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.nottaker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RecyclerView recView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(this.toolbar);
+
+        this.recView = findViewById(R.id.noteList);
     }
 }
--- app/src/main/java/com/example/nottaker/NoteDB.java
+++ app/src/main/java/com/example/nottaker/NoteDB.java
@@ -1,4 +1,47 @@
 package com.example.nottaker;
 
-public class NoteDB {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class NoteDB extends SQLiteOpenHelper {
+
+    // database info
+    private static final int DB_VERSION = 2;
+    private static final String DB_NAME = "ndb";
+    private static final String DB_TABLE = "ntable";
+
+    // table colums
+    private static final String KEY_ID = "id";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_CONTENT = "content";
+    private static final String KEY_DATE = "date";
+    private static final String KEY_TIME = "time";
+
+    NoteDB(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase con) {
+        String query = "CREATE TABLE " + DB_TABLE +"(";
+        query += KEY_ID + " INT PRIMARY KEY, ";
+        query += KEY_TITLE + " TEXT, ";
+        query += KEY_CONTENT + " TEXT, ";
+        query += KEY_DATE + " TEXT, ";
+        query += KEY_TIME + " TEXT";
+        query += ")";
+
+        con.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase con, int oldVer, int newVer) {
+        if (oldVer >= newVer) {
+            return;
+        }
+
+        con.execSQL("DROP TABLE IF EXISTS "+ DB_TABLE);
+    }
 }
