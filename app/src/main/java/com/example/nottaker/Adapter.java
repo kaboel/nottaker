55ae701c4ab2841256d0dbaaf440c0914e5b7d26
Faiq <faiq.kaboel@gmail.com>
2019-10-24 03:06:24 +0700

    data adapter

--- app/src/main/java/com/example/nottaker/Adapter.java
+++ app/src/main/java/com/example/nottaker/Adapter.java
@@ -1,4 +1,59 @@
 package com.example.nottaker;
 
-public class Adapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    private LayoutInflater inflater;
+    private List<Note> notes;
+
+    Adapter(Context context, List<Note> notes) {
+        this.inflater = LayoutInflater.from(context);
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.list_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+        String title = notes.get(position).getTitle();
+        String date = notes.get(position).getDate();
+        String time = notes.get(position).getTime();
+
+        holder.noteTitle.setText(title);
+        holder.noteDate.setText(date);
+        holder.noteTime.setText(time);
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView noteTitle, noteDate, noteTime;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            noteTitle = itemView.findViewById(R.id.noteTitle);
+            noteDate = itemView.findViewById(R.id.noteDate);
+            noteTime = itemView.findViewById(R.id.noteTime);
+        }
+    }
 }
